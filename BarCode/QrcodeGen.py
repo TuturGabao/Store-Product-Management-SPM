b3963a74adb046fa15b891161a308b8eac88d6ed
TuturGabao <129973798+TuturGabao@users.noreply.github.com>
2024-07-23 19:33:40 +0200

    Delete BarCode directory

--- BarCode/QrcodeGen.py
+++
@@ -1,7 +0,0 @@
-from barcode import EAN13
-from barcode.writer import ImageWriter
-
-def generate_barcode(number, file_name):
-    with open(file_name + ".jpeg", "wb") as f:
-        EAN13(number, writer=ImageWriter()).write(f)
-
